94b625fb8febc27fccdc79f781b464f301b03d94
Anna Do <annado@gmail.com>
2014-05-03 11:37:34 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   get 'sessions/create'
   match '/auth/yammer/callback', :to => 'sessions#create', :via => [:get]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
