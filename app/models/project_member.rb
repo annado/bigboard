ee323fe2c917150b45c2c8e2624c6bb6ea3fb122
Anna Do <annado@gmail.com>
2014-05-27 22:13:50 -0700

    ProjectMember requires person_id, styling changes to ProjectMembers

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -1,6 +1,7 @@
 class ProjectMember < ActiveRecord::Base
   has_paper_trail
   validates :project_id, presence: { message: "Requires project"}
+  validates :person_id, presence: { message: "Requires person"}
   belongs_to :project
   belongs_to :person
   belongs_to :role
