27c64f10715f9557426883cbc53c64acdbfb2b41
Anna Do <annado@gmail.com>
2014-06-09 16:56:40 -0700

    track ProjectMember changes by adding board_id

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -1,8 +1,12 @@
 class ProjectMember < ActiveRecord::Base
-  has_paper_trail
+  has_paper_trail :meta => { :board_id  => :board_identifier }
   validates :project_id, presence: { message: "Requires project"}
   validates :person_id, presence: { message: "Requires person"}
   belongs_to :project
   belongs_to :person
   belongs_to :role
+
+  def board_identifier
+    self.person.board_id
+  end
 end
