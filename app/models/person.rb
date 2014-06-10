e988cec33eecf8ece82f34957886e0c1b2cd1417
Anna Do <annado@gmail.com>
2014-06-10 13:52:01 -0700

    include Today in comparisons

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,8 +2,8 @@ class Person < ActiveRecord::Base
   has_paper_trail :meta => { :board_id  => :board_id }
   belongs_to :team
   belongs_to :board
-  has_many :project_members, -> { where ["project_members.start_date < ?", Date.today] }
-  has_many :projects, -> { where "projects.completed = ? AND projects.start_date < ?", false, Date.today }, :through => :project_members
+  has_many :project_members, -> { where ["project_members.end_date < ?", Date.today] }
+  has_many :projects, -> { where "projects.completed = ? AND projects.start_date <= ?", false, Date.today }, :through => :project_members
 
   validates :name, presence: { message: "Please specify a name"}
   validates :team, presence: true
