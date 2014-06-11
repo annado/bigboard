25a24e55b0f924e2e968207069539a32b3fc2152
Anna Do <annado@gmail.com>
2014-06-10 17:27:38 -0700

    exclude ProjectMembers with start date in the future

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   has_paper_trail :meta => { :board_id  => :board_id }
   belongs_to :team
   belongs_to :board
-  has_many :project_members, -> { where ["project_members.end_date > ? OR project_members.end_date IS NULL", Date.today] }
+  has_many :project_members, -> { where ["project_members.start_date <= ? AND (project_members.end_date > ? OR project_members.end_date IS NULL)", Date.today, Date.today] }
   has_many :projects, -> { where "projects.completed = ? AND projects.start_date <= ?", false, Date.today }, :through => :project_members
 
   validates :name, presence: { message: "Please specify a name"}
