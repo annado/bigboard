3d60a8ba43228ede5355a1c3c360a1413758b6b4
Anna Do <annado@gmail.com>
2014-06-12 17:41:48 -0700

    add null check for changelog items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,27 +42,27 @@ module ApplicationHelper
     if version.item_type == 'Project'
       item = Project.find_by_id version.item_id
       name = item.nil? ? '' : item.name
-      link = link_to name, board_project_path(board, item)
+      link = link_to name, board_project_path(board, item) unless item.nil?
     elsif version.item_type == 'ProjectMember'
       item = ProjectMember.find_by_id version.item_id
       name = item.nil? || item.person.nil? ? '': item.person.name
-      link = link_to name, project_member_path(item)
+      link = link_to name, project_member_path(item) unless item.nil?
     elsif version.item_type == 'Initiative'
       item = Initiative.find_by_id version.item_id
       name = item.nil? ? '' : item.name
-      link = link_to name, board_initiative_path(board, item)
+      link = link_to name, board_initiative_path(board, item) unless item.nil?
     elsif version.item_type == 'Person'
       item = Person.find_by_id version.item_id
       name = item.nil? ? '' : item.name
-      link = link_to name, board_person_path(board, item)
+      link = link_to name, board_person_path(board, item) unless item.nil?
     elsif version.item_type == 'Team'
       item = Team.find_by_id version.item_id
       name = item.nil? ? '' : item.name
-      link = link_to name, board_team_path(board, item)
+      link = link_to name, board_team_path(board, item) unless item.nil?
     elsif version.item_type == 'Board'
       item = Board.find_by_id version.item_id
       name = item.nil? ? '' : item.name
-      link = link_to name, board_team_path(board, item)
+      link = link_to name, board_team_path(board, item) unless item.nil?
     end
     link
   end
