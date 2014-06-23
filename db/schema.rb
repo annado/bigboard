d1d433ae3948f5af8cb2652b67a0d40b82e4c057
Joyce <jmhon08@gmail.com>
2014-06-23 16:58:25 -0700

    alert on project creation if start date is provided OR when start date is updated and no start date existed before. also make it so group id is not hard coded

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623052504) do
+ActiveRecord::Schema.define(version: 20140623234203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140623052504) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "group_id_for_yammer_post"
   end
 
   create_table "initiatives", force: true do |t|
