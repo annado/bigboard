fdc777f31a05cabaf7288879c353dabb26e343ff
Joyce <jmhon08@gmail.com>
2014-06-27 14:46:12 -0700

    differentiate between unassigned people and people on projects that haven't started yet

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,8 +3,7 @@ class Person < ActiveRecord::Base
   belongs_to :team
   belongs_to :board
   has_many :project_members, -> { where ["project_members.start_date <= ? AND (project_members.end_date > ? OR project_members.end_date IS NULL)", Date.today, Date.today] }, dependent: :destroy
-  has_many :projects, -> { where "projects.completed = ? AND projects.start_date <= ?", false, Date.today }, :through => :project_members
-
+  has_many :projects, :through => :project_members
   validates :name, presence: { message: "Please specify a name"}
   validates :team, presence: true
   validates_uniqueness_of :uid, scope: [:board_id], presence: { message: "Unique UID is required"}
@@ -12,7 +11,7 @@ class Person < ActiveRecord::Base
   searchkick autocomplete: ['name']
 
   def active_projects
-    self.projects
+    self.projects.where("projects.completed = ? AND projects.start_date <= ?", false, Date.today)
   end
 
   def active_project_count
@@ -25,6 +24,15 @@ class Person < ActiveRecord::Base
 
   def allocated_to_internal_initiative
     self.active_projects.first.initiative.name == 'Internal Projects' unless self.active_project_count == 0
+  end
+
+  def on_standing_initiative
+    self.projects.each do |proj|
+      if proj.initiative.standing
+        return true
+      end
+    end
+    return false
   end
 
 end
